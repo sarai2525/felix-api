24360e9e37dcb2c596d23e27794bf2181f42f9b1
sarai2525 <s.arai@newell-productions.com>
2023-06-24 20:19:36 +0900

    update: endpoint file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import express, {type Response} from 'express';
-import {consola} from 'consola';
+import express, { type Response } from 'express';
+import { consola } from 'consola';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import router from './router/index';
 
 const app = express();
 const port = 8000;
 app.use(express.json());
 
+// TODO: figure out how to fix: eslint-disable-next-line @typescript-eslint/no-unsafe-call
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use('/api', router);
 app.get('/', (_request, response: Response) => {
   response.send('Hello World!');
 });
