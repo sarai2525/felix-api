c691f7be38227f8cc2deb643dfbc712a5f7cb634
sarai2525 <s.arai@newell-productions.com>
2023-06-26 08:03:06 +0900

    fix: type

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { cert, getApp, getApps, initializeApp } from 'firebase-admin/app';
+import { cert, getApp, getApps, initializeApp, type App } from 'firebase-admin/app';
 
 const firebaseCert = {
   projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
@@ -7,7 +7,7 @@ const firebaseCert = {
   privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, '\n') // Ref: https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
 };
 
-const firebaseAdmin =
+const firebaseAdmin: App =
   getApps().length === 0
     ? initializeApp({
         credential: cert(firebaseCert)
