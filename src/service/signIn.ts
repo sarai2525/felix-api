b681ec4c261d3d8b76113ce5be647d6e32f8dfc6
sarai2525 <s.arai@newell-productions.com>
2023-06-26 03:20:33 +0900

    update: create user, login

--- src/service/signIn.ts
+++ src/service/signIn.ts
@@ -1,3 +1,8 @@
+import { PrismaClient } from '@prisma/client';
+import consola from 'consola';
+import dayjs from 'dayjs';
+import { getAuth, type UserRecord } from 'firebase-admin/auth';
+import firebaseAdmin from '../lib/firebaseAdmin.js';
 import FirebaseAuthClient from '../lib/firebaseAuthClient.js';
 
 interface User {
@@ -7,6 +12,7 @@ interface User {
   refreshToken: string;
   idToken: string;
   email: string;
+  role: string;
 }
 
 export default async function signIn({ email, password }: Record<string, string>): Promise<User> {
@@ -17,12 +23,33 @@ export default async function signIn({ email, password }: Record<string, string>
     registered,
     refreshToken
   } = await FirebaseAuthClient.postSignIn({ email, password });
+  const user: UserRecord = await getAuth(firebaseAdmin).getUser(publicId);
+  await updateLastLogin({ publicId });
   return {
     publicId,
     displayName,
     idToken,
     registered,
     refreshToken,
-    email
+    email,
+    role: user.customClaims!.role
   };
+}
+
+async function updateLastLogin({ publicId }): Promise<void> {
+  const prisma = new PrismaClient();
+  try {
+    await prisma.user.update({
+      where: {
+        publicId
+      },
+      data: {
+        lastLogin: dayjs().toISOString()
+      }
+    });
+  } catch (error) {
+    consola.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
--- src/service/signUp.ts
+++ src/service/signUp.ts
@@ -1,3 +1,8 @@
+import { PrismaClient } from '@prisma/client';
+import consola from 'consola';
+import { getAuth } from 'firebase-admin/auth';
+import { USER_ROLE } from '../constants/user.js';
+import firebaseAdmin from '../lib/firebaseAdmin.js';
 import FirebaseAuthClient from '../lib/firebaseAuthClient.js';
 
 interface User {
@@ -12,11 +17,37 @@ export default async function signUp({ email, password }: Record<string, string>
     email: emailAddress,
     refreshToken
   } = await FirebaseAuthClient.postSignUp({ email, password });
-
-  // Put response data to mysql database by prisma client
+  await setCustomRole(publicId, USER_ROLE.STAFF);
+  await createUser({ publicId, emailAddress });
   return {
     publicId,
     refreshToken,
     email: emailAddress
   };
+}
+
+async function setCustomRole(publicId, role): Promise<void> {
+  await getAuth(firebaseAdmin).setCustomUserClaims(publicId, {
+    role
+  });
+}
+
+async function createUser({ publicId, emailAddress }): Promise<void> {
+  const prisma = new PrismaClient();
+  try {
+    await prisma.user.create({
+      data: {
+        publicId,
+        email: emailAddress,
+        firstName: '',
+        lastName: '',
+        phoneNumber: '',
+        role: USER_ROLE.STAFF // TODO: be changeable
+      }
+    });
+  } catch (error) {
+    consola.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
