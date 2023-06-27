8cb35583f923e8271cee0f355d7c4fc292037e5a
sarai2525 <s.arai@newell-productions.com>
2023-06-27 20:39:28 +0900

    fix: path

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config.js'
 import { pino } from 'pino'
-import { LOG_LEVEL } from '../logLevel.js'
+import { LOG_LEVEL } from '../constants/logLevel.js'
 
 export const logger = pino({ level: process.env.LOG_LEVEL ?? LOG_LEVEL.INFO })
