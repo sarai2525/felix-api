81c4662ca28e05cad723cb0ff3ad954ae902e31c
sarai2525 <s.arai@newell-productions.com>
2023-06-27 19:51:53 +0900

    add: constants for deployment

---
+++ src/constants/logLevel.ts
@@ -0,0 +1,9 @@
+export const LOG_LEVEL = {
+  FATAL: 'fatal',
+  ERROR: 'error',
+  WARN: 'warn',
+  INFO: 'info', // production
+  DEBUG: 'debug',
+  TRACE: 'trace',
+  SILENT: 'silent'
+} as const
